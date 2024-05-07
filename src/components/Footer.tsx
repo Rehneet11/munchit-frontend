68db31437626edce6825fbded90c25531d8d110a
Rehneet11 <rstudy156@gmail.com>
2024-05-07 17:46:25 +0530

    Footer Colour changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer=()=>{
     
     return(
-        <div className="bg-slate-900 py-10">
+        <div className="bg-orange-400 py-10">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
             <span>
             <div className="flex items-center gap-3">
