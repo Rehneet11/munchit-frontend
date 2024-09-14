278ff0f79ebc64b79d5b4cf1b0cce3c125d47d4e
Rehneet Singh <118191754+Rehneet11@users.noreply.github.com>
2024-09-14 13:32:28 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import SearchBar, { SearchForm } from "@/components/SearchBar";
 import Appimg2 from "../assets/Appimg2.jpeg";
 import Appdownloadimg from "../assets/Appdownloadimg.png";
 import Carousel from "@/components/Carousel";
+import UserReview from "@/components/UserReview";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -23,6 +24,53 @@ const HomePage = () => {
         <SearchBar placeHolder="Search by city  " onSubmit={handleSearchSubmit} />
       </div>
       <Carousel/>
+      <UserReview
+  reviews={[
+    {
+      id: 1,
+      name: "John Doe",
+      rating: 5,
+      comment: "Great service and friendly staff!",
+      avatar: "https://randomuser.me/api/portraits/men/1.jpg",
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      rating: 4,
+      comment: "Loved the experience!",
+      avatar: "https://randomuser.me/api/portraits/women/1.jpg",
+    },
+    {
+      id: 3,
+      name: "Alex Johnson",
+      rating: 5,
+      comment: "Will definitely come back!",
+      avatar: "https://randomuser.me/api/portraits/men/2.jpg",
+    },
+    {
+      id: 4,
+      name: "Emily Davis",
+      rating: 4,
+      comment: "Good food but a bit pricey.",
+      avatar: "https://randomuser.me/api/portraits/women/2.jpg",
+    },
+    {
+      id: 5,
+      name: "Michael Lee",
+      rating: 5,
+      comment: "Best experience ever!",
+      avatar: "https://randomuser.me/api/portraits/men/3.jpg",
+    },
+    {
+      id: 6,
+      name: "Batman",
+      rating: 5,
+      comment: "Can recommend to a friend.",
+      avatar: "https://randomuser.me/api/portraits/men/4.jpg",
+    },
+  ]}/>
+
+
       <div className="bg-white p-10 rounded-lg">
         <div className="grid md:grid-cols-2 gap-5">
           <img
@@ -31,10 +79,10 @@ const HomePage = () => {
             className="w-auto h-auto max-h-[400px] mx-auto flex  items-center rounded-lg"
           />
           <div className="flex flex-col items-center justify-center gap-4 text-center">
-            <span className="font-bold text-3xl tracking-tighter text-slate-800">
+            <span className="font-bold text-3xl tracking-tighter text-slate-900">
               Order Now for Exclusive Deals
             </span>
-            <span>Download the App for 100Rs Off on First Order</span>
+            <span className="text-slate-600">Download the App for 100Rs Off on First Order</span>
             <Link to="/coming-soon">
               <img src={Appdownloadimg} alt="" />
             </Link>
@@ -47,3 +95,4 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
