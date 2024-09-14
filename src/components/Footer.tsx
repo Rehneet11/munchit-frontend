5c6c01a6697b23ac3872b2b98436a7ff0df0f72d
Rehneet Singh <118191754+Rehneet11@users.noreply.github.com>
2024-09-14 12:53:47 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
+
+
 import logomain from "../assets/logomain.jpg"
 import { Link } from "react-router-dom";
 const Footer=()=>{
     
     return(
-        <div className="bg-orange-400 py-10">
+        <div className="bg-red-500 py-10">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
             <span>
             <div className="flex items-center gap-3">
